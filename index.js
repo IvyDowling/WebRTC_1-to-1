ef5b8f1a741608571f6acb1f6956b0191e256f0d
iv <iv@ivDesktop>
2017-03-02 23:37:48 -0500

    began to migrate to jq, and implemented inmyroom function

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var socketIO = require('socket.io');
 
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
-  fileServer.serve(req, res);
+    fileServer.serve(req, res);
 }).listen(8080);
 
 var localRooms = {};
@@ -21,14 +21,24 @@ io.sockets.on('connection', function(client) {
     function log() {
         var array = ['Message from server:'];
         array.push.apply(array, arguments);
-        //console.log(array);
         client.emit('log', array);
+    }
+
+    function inMyRoom() {
+        var residents = localRooms[client.rooms[0]];
+        if (residents.length > 1) {
+            for (var c = 0; c < residents.length; c++) {
+                if (residents[c] === client.id) {
+                    residents.splice(c, 1);
+                }
+            }
+        }
     }
 
     client.on('message', function(message) {
         log("Client, " + client.id + " said:", message);
         // broadcast sends message to everyone besides this socket
-        if(client.visitingRoomName !== null) {
+        if (client.visitingRoomName !== null) {
             //we're visiting
             client.broadcast.to(client.visitingRoomName).emit('message', message);
         } else {
@@ -37,17 +47,17 @@ io.sockets.on('connection', function(client) {
         }
     });
 
-    client.on('create', function(room){
+    client.on('create', function(room) {
         //replaces client.rooms[0] as 'home room'
         //check if room is free
         if (localRooms[room] === undefined) {
             //leave old home room
-            if(client.rooms[0] !== undefined){
+            if (client.rooms[0] !== undefined) {
                 client.leave(client.rooms[0]);
             }
             //create room
             localRooms[room] = [client];
-            client.join(room, function(){
+            client.join(room, function() {
                 console.log("CREATED-ROOM-CALLBACK: client: " + client.id + "has created room " + room);
             });
             log("Client ID " + client.id + " created room " + room);
@@ -57,7 +67,7 @@ io.sockets.on('connection', function(client) {
 
     client.on('join', function(room) {
         //does this room exist
-        if(localRooms[room] !== undefined) {
+        if (localRooms[room] !== undefined) {
             //are we in this room
             if (room !== client.rooms[0] && room !== client.rooms[1]) {
                 //will we fit
@@ -67,7 +77,7 @@ io.sockets.on('connection', function(client) {
                         log("Client " + client.id + " is leaving room " + exit);
                         //--Leave current room--
                         //socket impl
-                        client.leave(client.rooms[1], function () {
+                        client.leave(client.rooms[1], function() {
                             console.log("LEAVE-ROOM-CALLBACK: client: " + client.id + " has now left the room " + exit);
                         });
                         //leave local room list
@@ -86,7 +96,7 @@ io.sockets.on('connection', function(client) {
                     //  this will help us quit that room on hangup
                     //  "visitingRoomName"
                     client.visitingRoomName = room;
-                    client.join(room, function () {
+                    client.join(room, function() {
                         console.log("JOIN-ROOM-CALLBACK: client: " + client.id + "has joined room " + room);
                         log('Client ID ' + client.id + ' joined clientId ' + room);
                         io.sockets.in(room).emit('join', room);
@@ -102,38 +112,21 @@ io.sockets.on('connection', function(client) {
         } else {
             log("client " + client.id + " tried to join room " + room + ", which does not exist.");
         }
-        //Server Logging
-        // console.log(">>>>>>>>>>>>>> client.id: " + client.id);
-        // console.log("io.sockets.sockets.length (total number of clients): " + io.sockets.sockets.length);
-        // console.log("Clientid: " + client.id + " client.visitingRoomName (custom variable to track room name): " + client.visitingRoomName);
-        // console.log("localRooms[" + room + "].length (number of clients in room): " + localRooms[room].length);
-        // var out = "";
-        // for (var r in localRooms[room]) {
-        //     out += localRooms[room][r].id + ", ";
-        // }
-        // console.log("room containts: " + out);
-        // out = "";
-        // for (var i in client.rooms) {
-        //     out += client.rooms[i].toString() + ", ";
-        // }
-        // console.log("----actual client.localRooms id---- " + out);
-        // console.log("");
-        //end logging
     });
 
-    client.on("leave", function(home){
+    client.on("leave", function(home) {
         log("Client " + client.id + " attempting to leave");
         var exit = client.rooms[1];
-        if(exit !== undefined) {
+        if (exit !== undefined) {
             //leave socket.io room
-            client.leave(client.rooms[1], function(){
+            client.leave(client.rooms[1], function() {
                 console.log("DISCONNECT-CALLBACK: client: " + client.id + " has now disconnected from room " + exit);
             });
             //leave local room list
-            if(client.visitingRoomName !== null){
+            if (client.visitingRoomName !== null) {
                 //find our id and remove us
-                for(var c = 0; c < localRooms[client.visitingRoomName].length; c++){
-                    if(localRooms[client.visitingRoomName][c].id === client.id){
+                for (var c = 0; c < localRooms[client.visitingRoomName].length; c++) {
+                    if (localRooms[client.visitingRoomName][c].id === client.id) {
                         localRooms[client.visitingRoomName].splice(c, 1);
                     }
                 }
@@ -152,11 +145,11 @@ io.sockets.on('connection', function(client) {
     client.on('ipaddr', function() {
         var ifaces = os.networkInterfaces();
         for (var dev in ifaces) {
-          ifaces[dev].forEach(function(details) {
-            if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-              client.emit('ipaddr', details.address);
-            }
-          });
+            ifaces[dev].forEach(function(details) {
+                if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+                    client.emit('ipaddr', details.address);
+                }
+            });
         }
     });
 });
--- js/main.js
+++ js/main.js
@@ -1,40 +1,45 @@
 'use strict';
 
-var isChannelReady = false;
 var owner = false;
 var isStarted = false;
 var localStream;
 var pc;
 var remoteStream;
-var pcConfig = {'iceServers': [{ 'url': 'stun:stun.l.google.com:19302'}]};
+// thanks google
+var pcConfig = {
+    'iceServers': [{
+        'url': 'stun:stun.l.google.com:19302'
+    }]
+};
 // Set up audio and video regardless
 var sdpConstraints = {
-    'mandatory': { 'OfferToReceiveAudio': true, 'OfferToReceiveVideo': true}
+    'mandatory': {
+        'OfferToReceiveAudio': true,
+        'OfferToReceiveVideo': true
+    }
 };
 
-//socket
-//server
 var clientId = prompt("User ID", "");
 console.log("USER ID: " + clientId);
 var socket = io.connect();
-if(document.querySelector("#callbuttonId") !== null){
-    document.querySelector("#callbuttonId").addEventListener("click", function() {
-        if(document.querySelector("#callfieldId") !== null){
-            var tocall = document.querySelector("#callfieldId").value;
-            if(tocall !== null && tocall !== "") {
+if ($("#callbuttonId").length > 0) {
+    $("#callbuttonId").click(function() {
+        if ($("#callfieldId").length > 0) {
+            var tocall = $("#callfieldId").value;
+            if (tocall !== null && tocall !== "") {
                 console.log("client attempting to join room: " + tocall);
                 socket.emit('join', tocall);
-                isChannelReady = true;
                 isStarted = false;
-                console.log('>>>>>>> maybeStart() ', "false: " + isStarted, localStream, "true: " + isChannelReady);
-                if (!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
+                if (!isStarted && typeof localStream !== 'undefined') {
                     createPeerConnection();
                     pc.addStream(localStream);
                     isStarted = true;
                     if (owner) {
                         console.log('creating offer for peer');
                         pc.createOffer(setLocalAndSendMessage,
-                            function(event){console.log('createOffer() error: ', event);},
+                            function(event) {
+                                console.log('createOffer() error: ', event);
+                            },
                             sdpConstraints
                         );
                     }
@@ -43,84 +48,87 @@ if(document.querySelector("#callbuttonId") !== null){
         }
     });
 }
-if(document.querySelector("#hangupbuttonId") !== null){
-    document.querySelector("#hangupbuttonId").addEventListener("click", hangup);
+
+if ($("#hangupbuttonId").length > 0) {
+    $("#hangupbuttonId").click(hangup);
 }
 
 if (clientId !== '') {
-  socket.emit('create', clientId);
-  console.log('Attempted to create clientId', clientId);
+    socket.emit('create', clientId);
+    console.log('Attempted to create room: ', clientId);
 }
 
 socket.on('created', function(room) {
-  console.log('Created clientId ' + room);
-  owner = true;
+    console.log('Created clientId ' + room);
+    owner = true;
 });
 
 socket.on('full', function(room) {
-  console.log('Room ' + room + ' is full');
+    console.log('Room ' + room + ' is full');
 });
 
-socket.on('join', function (room){
+socket.on('join', function(room) {
     console.log('Another peer made a request to join room ' + room);
-    console.log('This peer is the initiator of room ' + room + '!');
     console.log('Sending answer to peer.');
     pc.createAnswer().then(
         setLocalAndSendMessage,
-        function(error){console.log('Failed to create session description: ' + error.toString());},
+        function(error) {
+            console.log('Failed to create session description: ' + error.toString());
+        },
         sdpConstraints
     );
-    isChannelReady = true;
 });
 
 socket.on('joined', function(room) {
-  console.log('joined: ' + room);
-  isChannelReady = true;
+    console.log('joined: ' + room);
 });
 
 socket.on('log', function(array) {
-  console.log.apply(console, array);
+    console.log.apply(console, array);
 });
+
+socket.on('')
 
 //client
 
 function sendMessage(message) {
-  console.log('Client sending message: ', message);
-  socket.emit('message', message);
+    console.log('Client sending message: ', message);
+    socket.emit('message', message);
 }
 
 // This client receives a message
 socket.on('message', function(message) {
     console.log('Server says::', message);
     if (message === 'got user media') {
-        console.log('>>>>>>> maybeStart() ', "false: " + isStarted, localStream, "true: " + isChannelReady);
-        if (!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
+        if (!isStarted && typeof localStream !== 'undefined') {
             createPeerConnection();
             pc.addStream(localStream);
             isStarted = true;
             if (owner) {
                 console.log('creating offer for peer');
                 pc.createOffer(setLocalAndSendMessage,
-                    function(event){console.log('createOffer() error: ', event);},
+                    function(event) {
+                        console.log('createOffer() error: ', event);
+                    },
                     sdpConstraints
                 );
             }
         }
     } else if (message.type === 'offer') {
-        if(confirm("received offer, answer?")) {
+        if (confirm("received offer, answer?")) {
             socket.emit('join', clientId);
-            isChannelReady = true;
             isStarted = false;
             owner = false;
-            console.log('>>>>>>> maybeStart() ', "false: " + isStarted, localStream, "true: " + isChannelReady);
-            if (!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
+            if (!isStarted && typeof localStream !== 'undefined') {
                 createPeerConnection();
                 pc.addStream(localStream);
                 isStarted = true;
                 if (owner) {
                     console.log('creating offer for peer');
                     pc.createOffer(setLocalAndSendMessage,
-                        function(event){console.log('createOffer() error: ', event);},
+                        function(event) {
+                            console.log('createOffer() error: ', event);
+                        },
                         sdpConstraints
                     );
                 }
@@ -129,7 +137,9 @@ socket.on('message', function(message) {
             console.log('Sending answer to peer.');
             pc.createAnswer().then(
                 setLocalAndSendMessage,
-                function(error){console.log('Failed to create session description: ' + error.toString());},
+                function(error) {
+                    console.log('Failed to create session description: ' + error.toString());
+                },
                 sdpConstraints
             );
         }
@@ -148,39 +158,48 @@ socket.on('message', function(message) {
 
 //GetUserMedia
 //here is a good place to do browser checks for ie and safari (not supported)
-navigator.mediaDevices.getUserMedia({audio: false, video: true})
+navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: true
+    })
     .then(function(stream) {
         var localVideo = document.querySelector('#localVideo');
         localVideo.src = window.URL.createObjectURL(stream);
         localStream = stream;
         sendMessage('got user media');
-        if (owner && !isStarted && typeof localStream !== 'undefined' && isChannelReady) {
+        if (owner && !isStarted && typeof localStream !== 'undefined') {
             createPeerConnection();
             pc.addStream(localStream);
             isStarted = true;
             if (owner) {
                 console.log('creating offer for peer');
                 pc.createOffer(setLocalAndSendMessage,
-                    function(event){console.log('createOffer() error: ', event);},
+                    function(event) {
+                        console.log('createOffer() error: ', event);
+                    },
                     sdpConstraints
                 );
             }
         }
-    }).catch(function(e) { alert('getUserMedia() error: ' + e.name);});
+    }).catch(function(e) {
+        alert('Error when getting video source: ' + e.name);
+    });
 
 //PEER CONNECTION
 
 function createPeerConnection() {
-  try {
-    pc = new RTCPeerConnection(null);
-    pc.onicecandidate = handleIceCandidate;
-    pc.onaddstream = handleRemoteStreamAdded;
-    pc.onremovestream = function(event){console.log('Remote stream removed. Event: ', event);};
-    console.log('Created RTCPeerConnnection');
-  } catch (e) {
-    console.log('Failed to create PeerConnection, exception: ' + e.message);
-    alert('Cannot create RTCPeerConnection object.');
-  }
+    try {
+        pc = new RTCPeerConnection(null);
+        pc.onicecandidate = handleIceCandidate;
+        pc.onaddstream = handleRemoteStreamAdded;
+        pc.onremovestream = function(event) {
+            console.log('Remote stream removed. Event: ', event);
+        };
+        console.log('Created RTCPeerConnnection');
+    } catch (e) {
+        console.log('Failed to create PeerConnection, exception: ' + e.message);
+        alert('Cannot create RTCPeerConnection object.');
+    }
 }
 
 function handleIceCandidate(event) {
@@ -198,16 +217,16 @@ function handleIceCandidate(event) {
 }
 
 function handleRemoteStreamAdded(event) {
-    var remoteVideo = document.querySelector('#remoteVideo');
+    var remoteVideo = $('#remoteVideo');
     console.log('Remote stream added.');
     remoteVideo.src = window.URL.createObjectURL(event.stream);
     remoteStream = event.stream;
 }
 
 function setLocalAndSendMessage(sessionDescription) {
-  pc.setLocalDescription(sessionDescription);
-  console.log('setLocalAndSendMessage sending message', sessionDescription);
-  sendMessage(sessionDescription);
+    pc.setLocalDescription(sessionDescription);
+    console.log('setLocalAndSendMessage sending message', sessionDescription);
+    sendMessage(sessionDescription);
 }
 
 function hangup() {
@@ -226,7 +245,7 @@ function stop() {
     owner = true;
     //if room host, will do nothing
     socket.emit("leave", clientId);
-    if(pc !== null && pc !== undefined) {
+    if (pc !== null && pc !== undefined) {
         pc.close();
         pc = null;
     }
